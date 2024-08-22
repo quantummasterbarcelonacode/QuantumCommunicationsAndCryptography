00fdc4f9c257e85c7e6556d020f87dc33c547e31
Lumorti <lbmortimer1998@gmail.com>
2024-08-22 15:10:27 +0200

    remove the file extensions from the pages

--- compileAll.sh
+++ compileAll.sh
@@ -46,3 +46,25 @@ mv esMd/_build/html es
 mv frMd/_build/html fr
 rm -r enMd/_build esMd/_build frMd/_build
 rm -r en/_sources es/_sources fr/_sources
+
+# For each of the built html files
+for file in $(find . -wholename '*/content/*.html'); do
+
+    echo "Removing html links from $file"
+    sed -i 's/chapter1.html/chapter1/g' $file
+    sed -i 's/chapter2.html/chapter2/g' $file
+    sed -i 's/chapter3.html/chapter3/g' $file
+    sed -i 's/chapter4.html/chapter4/g' $file
+    sed -i 's/chapter5.html/chapter5/g' $file
+    sed -i 's/chapter6.html/chapter6/g' $file
+    sed -i 's/chapter7.html/chapter7/g' $file
+    sed -i 's/chapter8.html/chapter8/g' $file
+    sed -i 's/chapter9.html/chapter9/g' $file
+    sed -i 's/section1.html/section1/g' $file
+    sed -i 's/section2.html/section2/g' $file
+    sed -i 's/section3.html/section3/g' $file
+    sed -i 's/section4.html/section4/g' $file
+    sed -i 's/section5.html/section5/g' $file
+    sed -i 's/section6.html/section6/g' $file
+
+done
